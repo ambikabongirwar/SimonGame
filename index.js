4377caee19b89bbe526cf0e519aeffe85f2d6686
ambikabongirwar@gmail.com <ambikabongirwar@gmail.com>
2021-12-18 20:20:14 +0530

    realised that for loop can not be used to take inputs in front end

--- index.js
+++ index.js
@@ -1,79 +1,84 @@
-
-var buttonColours = ["red", "blue", "green", "yellow"];
-
-var gamePattern = [];
-var userClickedPattern = [];
-
-var started = false;
-var level = 0;
-
-$(document).keypress(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
-
-$(".btn").click(function() {
-
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.length-1);
-});
-
-function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
-    }
-}
-
+let audio, level = 1, isFirstTime = 1;
+let gamePattern = []
+let buttonColors = ["green", "red", "yellow", "blue"]
 
 function nextSequence() {
-  userClickedPattern = [];
-  level++;
-  $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColour);
+    let randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColour = buttonColors[randomNumber]
+    gamePattern.push(randomChosenColour);
+    animatePress(randomChosenColour)
+    playSound(randomChosenColour)
 }
 
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
-}
+/*for (var i = 0; i < 4; i++) {
+    nextSequence();
+    for (var j = 0; j < gamePattern.length; j++) {
+        $("." + gamePattern[j]).fadeOut().fadeIn();
+        audio = new Audio("sounds/" + gamePattern[j] + ".mp3");
+        audio.play();
+    }
+}*/
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
+    audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
+function animatePress(colorChosen) {
+    $("." + colorChosen).addClass("pressed");
+    setTimeout(function() {$("." + colorChosen).removeClass("pressed");}, 100)
 }
+
+function handleButtonClick(userClickedPattern) {
+    for (var i = 0; i < level; i++) {
+        $(".btn").click(function() {
+            userChosenColor = this.classList[1];
+            animatePress(userChosenColor)
+            playSound(userChosenColor)
+            pattern.push(userChosenColor)
+            //console.log(userClickedPattern)
+        })
+    }
+    return userClickedPattern;
+}
+
+function gameOver() {
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    playSound("wrong")
+    $("body").addClass("game-over");
+    setTimeout(function() {$("body").removeClass("game-over")}, 200);
+    gamePattern = [];
+    isFirstTime = 1;
+}
+
+function isEqual(userClickedPattern) {
+    for (var i = 0; i < level; i++) {
+        if (gamePattern[i] != userClickedPattern[i]) {
+            gameOver();
+            return 0;
+        }
+    }
+    return 1;
+}
+
+function startGame() {
+    do {
+        console.log("inside do");
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        var pattern = [];
+        level++;
+        console.log(gamePattern);
+        pattern = handleButtonClick(pattern);
+        console.log(pattern);
+        console.log("is Equal: " + isEqual(pattern));
+    } while(isEqual(pattern) === 1 && level < 50);
+}
+
+$(document).keypress(function(){
+    if (isFirstTime === 1) {
+        isFirstTime = 0;
+        console.log("key pressed");
+        startGame();
+    }
+});
